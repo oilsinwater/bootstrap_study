9840334d3ad902aabad66958b4224d1f2e7496d8
Phil <philliphdrummond@gmail.com>
2016-11-27 00:19:20 -0800

    following along with tutorial...

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+   process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
    var express = require('./config/express');
 
    var app = express();
-
    app.listen(3000);
    module.exports = app;
-
    console.log('Running...http://localhost:3000/');
