5fce453db2693db5180daae76af626a207556243
Phil <philliphdrummond@gmail.com>
2016-11-27 01:29:05 -0800

    Adds more js files

--- config/env/development.js
+++ config/env/development.js
@@ -1,3 +1,4 @@
-  module.exports = {
-     // Development configuration options
-};
+module.exports = {
+     db: 'mongodb://localhost/mean-book',
+     sessionSecret: 'developmentSessionSecret'
+   };
