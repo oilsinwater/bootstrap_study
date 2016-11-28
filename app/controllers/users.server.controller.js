8e358cea3d8c7db0bd88118ea764b1d2f8ef116e
Phil <philliphdrummond@gmail.com>
2016-11-27 18:20:44 -0800

    Modifies Json

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -19,3 +19,36 @@ exports.list = function(req, res, next) {
          res.json(users);
 } });
 };
+
+exports.read = function(req, res) {
+     res.json(req.user);
+};
+   exports.userByID = function(req, res, next, id) {
+     User.findOne({
+       _id: id
+     }, function(err, user) {
+       if (err) {
+         return next(err);
+       } else {
+         req.user = user;
+         next();
+} });
+};
+
+exports.update = function(req, res, next) {
+     User.findByIdAndUpdate(req.user.id, req.body, function(err, user) {
+       if (err) {
+         return next(err);
+       } else {
+         res.json(user);
+} });
+};
+
+ exports.delete = function(req, res, next) {
+     req.user.remove(function(err) {
+       if (err) {
+         return next(err);
+            } else {
+         res.json(req.user);
+} })
+};
--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,13 +1,29 @@
-
-  var mongoose = require('mongoose'),
+ var mongoose = require('mongoose'),
        Schema = mongoose.Schema;
 
    var UserSchema = new Schema({
+        website: {
+        type: String,
+        get: function(url) {
+      if (!url) {
+        return url;
+      } else {
+      if (url.indexOf('http://') !== 0
+      && url.indexOf('https://') !== 0) {
+                url = 'http://' + url;
+        }
+        return url; }
+      }
+    },
      firstName: String,
      lastName: String,
      email: String,
-     username: String,
-     password: String
+     username: String, password: String,
+     created: {
+       type: Date,
+       default: Date.now
+     }
    });
-
+   
+   UserSchema.set('toJSON', { getters: true });
    mongoose.model('User', UserSchema);
