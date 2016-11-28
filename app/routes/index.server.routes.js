5401b8d87527e238257fecff6a5e538a36cd8065
Phil <philliphdrummond@gmail.com>
2016-11-27 19:45:13 -0800

    adds changes to authentication

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,5 +1,6 @@
-
-module.exports = function(app) {
-    var index = require('../controllers/index.server.controller.js');
-    app.get('/', index.render);
-};
+exports.render = function(req, res) {
+     res.render('index', {
+       title: 'Hello World',
+       userFullName: req.user ? req.user.fullName : ''
+     });
+   };
